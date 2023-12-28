1ea5836a8cad3f191eb4e30951194cc510410655
Kaleb Matherne <kalebmatmat@gmail.com>
2023-12-28 15:49:16 -0600

    update all urls

--- Formula/ipterm.rb
+++ Formula/ipterm.rb
@@ -3,10 +3,10 @@
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Ipterm < Formula
   desc "Simple ip lookup terminal tool written in rust"
-  homepage "https://github.com/kmatherne/ip-term"
-  url "https://github.com/kmatherne/ipterm/releases/download/v0.1.0/ip-term-mac.tar.gz"
-  sha256 "35ef3f1d2fdb5625f9c578c26adcf39581c0cdfe0c72466859265d5b10f5e8d0"
-  version "0.1.0"
+  homepage "https://github.com/kmatherne/ipterm"
+  url "https://github.com/kmatherne/ipterm/releases/download/v0.1.1/ipterm-mac.tar.gz"
+  sha256 "346fddf0127c5579e9c50687d0013d495e888d5c1f122a1a8ff8ae35ea8c0026"
+  version "0.1.1"
 
   def install
     bin.install "ipterm"
