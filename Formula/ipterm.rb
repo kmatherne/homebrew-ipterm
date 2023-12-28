ad34854a0b7cbe2e3cc80230e0f1856b74bb399c
Kaleb Matherne <kalebmatmat@gmail.com>
2023-12-28 15:40:40 -0600

    update git repo location

--- Formula/ipterm.rb
+++ Formula/ipterm.rb
@@ -4,7 +4,7 @@
 class Ipterm < Formula
   desc "Simple ip lookup terminal tool written in rust"
   homepage "https://github.com/kmatherne/ip-term"
-  url "https://github.com/kmatherne/ip-term/releases/download/v0.1.0/ip-term-mac.tar.gz"
+  url "https://github.com/kmatherne/ipterm/releases/download/v0.1.0/ip-term-mac.tar.gz"
   sha256 "35ef3f1d2fdb5625f9c578c26adcf39581c0cdfe0c72466859265d5b10f5e8d0"
   version "0.1.0"
 
